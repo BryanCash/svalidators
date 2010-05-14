601cb6ee8dc21d541a20db3c15f9bbd486d6f6f5
spyros@valor.gr <spyros@valor.gr@8f1ab777-d734-9c7b-dde6-6b1e2bdff621>
2010-05-14 05:29:17 +0000

    Added exceptions javadoc

--- trunk/com/googlecode/svalidators/exceptions/ComponentException.java
+++ trunk/com/googlecode/svalidators/exceptions/ComponentException.java
@@ -2,22 +2,27 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.googlecode.svalidators.exceptions;
 
 /**
- *
+ * ComponentException<br />
+ * Is thrown when an invalid component is added to a validation group
  * @author ssoldatos
  */
 public class ComponentException extends IllegalArgumentException {
 
+  /**
+   * Default Constructor
+   */
   public ComponentException() {
     super();
   }
 
-  public ComponentException(String string) {
-    super(string);
+  /**
+   * Constructor with the exceptions message
+   * @param message The exceptions message
+   */
+  public ComponentException(String message) {
+    super(message);
   }
-
-  
 }
