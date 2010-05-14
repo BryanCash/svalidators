74baf7bf99d3ec0dd1372e5edc91202db239c87c
spyros@valor.gr <spyros@valor.gr@8f1ab777-d734-9c7b-dde6-6b1e2bdff621>
2010-05-14 06:18:48 +0000

    Added formcomponents  javadoc

--- com/googlecode/svalidators/formcomponents/ValidationGroup.java
+++ com/googlecode/svalidators/formcomponents/ValidationGroup.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.googlecode.svalidators.formcomponents;
 
 import java.util.ArrayList;
@@ -11,51 +10,84 @@ import javax.swing.JOptionPane;
 import com.googlecode.svalidators.exceptions.ComponentException;
 
 /**
- *
+ * A ValidationGroup consists of a list of components to be validated
  * @author ssoldatos
  */
 public class ValidationGroup {
+
+  /**
+   * The list of components to validate
+   */
   ArrayList<JComponent> components = new ArrayList<JComponent>();
 
+  /**
+   * Creates an empty ValidationGroup
+   */
   public ValidationGroup() {
   }
 
-  public ValidationGroup(ArrayList<JComponent> components){
+  /**
+   * Creates a ValidationGroup with a list of components to validate
+   * @param components
+   */
+  public ValidationGroup(ArrayList<JComponent> components) {
     for (JComponent jComponent : components) {
       addComponent(jComponent);
     }
   }
 
-  public void addComponent(JComponent component) throws ComponentException{
-    if(component instanceof SComboBox || component instanceof STextField){
+  /**
+   * Adds a component to the ValidationGroup
+   * @param component The component to add. Must be {@link STextField} or
+   * {@link SComboBox}
+   * @throws ComponentException If the component is not {@link STextField} or
+   * {@link SComboBox} 
+   */
+  public void addComponent(JComponent component) throws ComponentException {
+    if (component instanceof SComboBox || component instanceof STextField) {
       components.add(component);
     } else {
       throw new ComponentException("Not valid component");
     }
   }
 
-  public void removeComponent(JComponent component){
+  /**
+   * Removes a component from the {@link #components}
+   * @param component
+   */
+  public void removeComponent(JComponent component) {
     components.remove(component);
   }
 
-  public void clear(){
+  /**
+   * Clears the {@link #components} list
+   */
+  public void clear() {
     components.clear();
   }
 
-  public int getSize(){
+  /**
+   * Gets the number of components in {@link #components}
+   * @return The number of components
+   */
+  public int getSize() {
     return components.size();
   }
 
-  public boolean validate(){
+  /**
+   * Validate the components in {@link #components}
+   * @return true if validation succeeds else false
+   */
+  public boolean validate() {
     for (JComponent jComponent : components) {
-      if (jComponent instanceof SComboBox){
-        SComboBox combo = (SComboBox )jComponent;
-        if(!combo.validateValue()){
+      if (jComponent instanceof SComboBox) {
+        SComboBox combo = (SComboBox) jComponent;
+        if (!combo.validateValue()) {
           return false;
         }
-      }else if (jComponent instanceof STextField){
-        STextField textfield = (STextField )jComponent;
-        if(!textfield.validateValue()){
+      } else if (jComponent instanceof STextField) {
+        STextField textfield = (STextField) jComponent;
+        if (!textfield.validateValue()) {
           return false;
         }
       }
@@ -63,12 +95,17 @@ public class ValidationGroup {
     return true;
   }
 
+  /**
+   * Displays a message when validation fails
+   */
   public void errorMessage() {
     JOptionPane.showMessageDialog(null, "The form validation failed.\nCheck the errors displayed in the form.", "Validation failed", JOptionPane.ERROR_MESSAGE);
   }
 
+  /**
+   * Displays a message when validation succeeds
+   */
   public void successMessage() {
     JOptionPane.showMessageDialog(null, "Validation succeeded", "Success", JOptionPane.INFORMATION_MESSAGE);
   }
-
 }
--- com/googlecode/svalidators/formcomponents/ValidatorList.java
+++ com/googlecode/svalidators/formcomponents/ValidatorList.java
@@ -10,32 +10,57 @@ import java.util.Map;
 import com.googlecode.svalidators.validators.SValidator;
 
 /**
- *
+ * A list of a components Validators
  * @author ssoldatos
  */
 public class ValidatorList {
   Map<String,SValidator> validators = new HashMap<String, SValidator>();
 
+  /**
+   * Creates a default list
+   */
   public ValidatorList() {
   }
 
+  /**
+   * Adds a validator to the {@link #validators}. Only one validator of each type
+   * is allowed in the list, so if there's already a validator of this type in the list
+   * is removed first.
+   * @param validator The validator to add
+   */
   public void addValidator(SValidator validator){
     removeValidator(validator.getType());
     validators.put(validator.getType(), validator);
   }
 
+  /**
+   * Removes a validator from the {@link #validators}
+   * @param type The type of validator to remove
+   */
   public void removeValidator(String type){
     validators.remove(type);
   }
 
+  /**
+   * Clears the  {@link #validators} list
+   */
   public void clearValidators(){
     validators.clear();
   }
 
+  /**
+   * Gets the  {@link #validators} size
+   * @return The number of validators in  {@link #validators}
+   */
   public int getSize(){
     return validators.size();
   }
 
+ /**
+  * Get a validsator from the list
+  * @param type The type of validator to get
+  * @return The validator
+  */
   public SValidator getValidator(String type) {
     return validators.get(type);
   }
