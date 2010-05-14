984b4acc86be777115006a595878caf78ada1915
spyros@valor.gr <spyros@valor.gr@8f1ab777-d734-9c7b-dde6-6b1e2bdff621>
2010-05-14 05:29:17 +0000

    Added exceptions javadoc

--- com/googlecode/svalidators/exceptions/AttributeException.java
+++ com/googlecode/svalidators/exceptions/AttributeException.java
@@ -6,16 +6,24 @@
 package com.googlecode.svalidators.exceptions;
 
 /**
- *
+ * AttributeException<br />
+ * Is thrown when a invalid attribute is set to a validator
  * @author ssoldatos
  */
 public class AttributeException extends IllegalArgumentException {
   private static final long serialVersionUID = 235364575687L;
 
+  /**
+   * Default Constructor
+   */
  public AttributeException() {
     super();
   }
 
+ /**
+  * Constructor with the exceptions message
+  * @param message The exceptions message
+  */
   public AttributeException(String message) {
     super(message);
   }
